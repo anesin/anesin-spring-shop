7b0024370835e485a5ce21f9a61d0ffc2e4aa53f
이재영 <jaeyoung.yi@seculetter.com>
2021-12-13 00:12:05 +0900

    3장 Thymeleaf 학습하기   3.3 Thymeleaf 예제 진행하기
    - th:text
    - th:each
    - th:if, th:unless
    - th:switch, th:case
    - th:href

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,14 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -14,6 +19,67 @@ public class ThymeleafExController {
   public String thymeleafExample01(Model model) {
     model.addAttribute("data", "thymeleaf example");
     return "thymeleafEx/thymeleafEx01";
+  }
+
+
+  @GetMapping(value = "/ex02")
+  public String thymeleafExample02(Model model) {
+    ItemDto itemDto = new ItemDto();
+    itemDto.setItemDetail("product detail");
+    itemDto.setItemNm("test product1");
+    itemDto.setPrice(10000);
+    itemDto.setRegTime(LocalDateTime.now());
+    model.addAttribute("itemDto", itemDto);
+    return "thymeleafEx/thymeleafEx02";
+  }
+
+
+  @GetMapping(value = "/ex03")
+  public String thymeleafExample03(Model model) {
+    makeItemDtoList(model);
+    return "thymeleafEx/thymeleafEx03";
+  }
+
+
+  @GetMapping(value = "/ex04")
+  public String thymeleafExample04(Model model) {
+    makeItemDtoList(model);
+    return "thymeleafEx/thymeleafEx04";
+  }
+
+
+  @GetMapping(value = "/ex04-1")
+  public String thymeleafExample04_1(Model model) {
+    makeItemDtoList(model);
+    return "thymeleafEx/thymeleafEx04-1";
+  }
+
+
+  private void makeItemDtoList(Model model) {
+    List<ItemDto> itemDtoList = new ArrayList<>();
+    for (int i = 1; i <= 10; ++i) {
+      ItemDto itemDto = new ItemDto();
+      itemDto.setItemDetail("product detail" + i);
+      itemDto.setItemNm("test product" + i);
+      itemDto.setPrice(1000 * i);
+      itemDto.setRegTime(LocalDateTime.now());
+      itemDtoList.add(itemDto);
+    }
+    model.addAttribute("itemDtoList", itemDtoList);
+  }
+
+
+  @GetMapping(value = "/ex05")
+  public String thymeleafExample05(Model model) {
+    return "thymeleafEx/thymeleafEx05";
+  }
+
+
+  @GetMapping(value = "/ex06")
+  public String thymeleafExample06(String param1, String param2, Model model) {
+    model.addAttribute("param1", param1);
+    model.addAttribute("param2", param2);
+    return "thymeleafEx/thymeleafEx06";
   }
 
 }
